613e3a6c593744a20a620bfa4b8cebcb625cd1ff
Juanjo González <juanjo96developer@gmail.com>
2026-08-31 11:22:16 +0200

    fix(coverage): pin summary output path

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'json-summary' },
+        { type: 'json-summary', file: 'coverage-summary.json' },
         { type: 'text-summary' },
       ],
     },
